37b0c06c0222eca9c925d4b0dd0be7c84b742659
Thomas J Dalby <thomasjackdalby@gmail.com>
2015-04-11 17:31:29 +0100

    control types

---
+++ WillItLaunch/src/willitlaunch/controllers/InputGridController.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package willitlaunch.controllers;
+
+/**
+ *
+ * @author Tom
+ */
+public class InputGridController {
+    
+    // This controller is responsible for putting widgets into the grid
+    
+    
+    public void createInputWidget(InputType type, int row, int column)
+    {
+        throw new NotSupportedException();
+    }
+    
+    
+    
+    
+    
+}
