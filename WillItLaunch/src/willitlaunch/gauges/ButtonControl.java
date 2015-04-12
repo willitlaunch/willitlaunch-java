573c61b348bd87a354083888c94860e49d9834b8
Ben Oxley <ben.oxley@gmail.com>
2015-04-12 01:00:24 +0100

    Created button control

---
+++ WillItLaunch/src/willitlaunch/gauges/ButtonControl.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package willitlaunch.gauges;
+
+import org.json.JSONObject;
+
+/**
+ *
+ * @author ben
+ */
+public class ButtonControl extends GaugeBase {
+    
+    public ButtonControl (int id, String label){
+        super(id);
+    }
+    
+    @Override
+    protected void configureGauge() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void update(JSONObject obj) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    protected void callServer() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
